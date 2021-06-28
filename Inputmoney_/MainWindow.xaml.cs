fef0654fc11a56f81c9ce19d6edf9a0c71bc3e33
Diana_B <diana.butorina2002@mail.ru>
2021-06-28 10:48:36 +0700

    Функция клика

--- Inputmoney_/MainWindow.xaml.cs
+++ Inputmoney_/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Inputmoney_
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int money = Convert.ToInt32(Money.Text);
+        }
     }
 }
